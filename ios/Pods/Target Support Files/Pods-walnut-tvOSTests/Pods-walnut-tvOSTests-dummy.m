e129aef569e32ef45a99ec3fe1318287d3f54fe0
Gianfranco Palumbo <gianpa@gmail.com>
2018-10-30 14:21:25 +0200

    add react-native-firebase and RNFirebaseAnalytics

---
+++ ios/Pods/Target Support Files/Pods-walnut-tvOSTests/Pods-walnut-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_walnut_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_walnut_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-walnut/Pods-walnut-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_walnut : NSObject
+@end
+@implementation PodsDummy_Pods_walnut
+@end
