94a4bb5a786cee445e8bd5690ed8260a8c0721f4
Gianfranco Palumbo <gianpa@gmail.com>
2018-11-04 19:35:35 +0200

    update entries

--- static/entries.js
+++ static/entries.js
@@ -1,113 +1,53 @@
 export const channels = [
   {
-    title: "learn",
-    subreddit: "watchandlearn",
-    textColor: "",
-    bgColor: "#0087ff"
-  },
-  {
-    title: "hot",
+    title: "general",
     subreddit: "videos",
     textColor: "white",
-    bgColor: "#ed0b22"
+    minNumOfVotes: 0
   },
   {
     title: "curious",
-    subreddit: "curiousvideos;mealtimevideos;educativevideos",
+    subreddit: "curiousvideos;mealtimevideos;educativevideos;watchandlearn",
     textColor: "",
-    bgColor: "#ff9000"
+    minNumOfVotes: 0
   },
   {
     title: "how to",
     subreddit:
-      "maker;howto;TechDIY;upcycling;DIY;easyfix;woodworking;FastWorkers;lifehacks",
+      "maker;howto;artisanvideos;TechDIY;upcycling;DIY;easyfix;woodworking;FastWorkers;lifehacks",
     textColor: "",
-    bgColor: "#1f373d"
+    minNumOfVotes: 0
   },
   {
-    title: "artisan",
-    subreddit: "artisanvideos",
+    title: "educational",
+    subreddit: "physics;math;psychology;space;biology",
     textColor: "",
-    bgColor: "#ff69b4"
+    minNumOfVotes: 0
+  },
+  {
+    title: "documentaries",
+    subreddit: "documentaries",
+    textColor: "",
+    minNumOfVotes: 0
+  },
+  {
+    title: "lectures",
+    subreddit: "lectures",
+    textColor: "",
+    minNumOfVotes: 0
   },
   {
     title: "food",
-    // cookingvideos?
     subreddit:
       "veganrecipes;FoodVideos;foodhacks;vegan;juicing;foodhacks;beekeeping;homebrewing;GuerrillaGardening;Gardening",
     textColor: "",
-    bgColor: "#269108"
-  },
-  {
-    title: "health",
-    subreddit: "yoga;flexibility;WorldStreetWorkout;meditation",
-    textColor: "",
-    bgColor: "#ff4238"
+    minNumOfVotes: 0
   },
   {
     title: "outdoors",
     subreddit:
       "hiking;trailrunning;OutdoorHacks;adrenaline;WildernessBackpacking;climbing;backpacking;The_Great_Outdoors;CampingandHiking;hiking;trailmeals;myog;Kayaking;OpenWaterSwimming;surfing",
     textColor: "",
-    bgColor: "#1ddb00"
-  },
-  {
-    title: "educational",
-    subreddit: "physics;math;psychology;space;biology",
-    textColor: "",
-    bgColor: "#0868d6"
-  },
-  {
-    title: "documentaries",
-    subreddit: "documentaries",
-    textColor: "",
-    bgColor: "#8dbbe0"
-  },
-  {
-    title: "lectures",
-    subreddit: "lectures",
-    textColor: "",
-    bgColor: "#2308a8"
-  },
-  {
-    title: "animaltherapy",
-    subreddit:
-      "aww;cats;animalsbeingbros;awwducational;dogs;thisismylifenow;blep;eyebeach;tippytaps;awww",
-    textColor: "",
-    bgColor: "#ffff00"
+    minNumOfVotes: 0
   }
-
-  // {
-  //   title: "music",
-  //   subreddit: "music",
-  //   textColor: "",
-  //   bgColor: "#c808d6"
-  // },
-
-  // {
-  //   title: "funny",
-  //   subreddit: "contagiouslaughter",
-  //   textColor: "",
-  //   bgColor: "#e8d402"
-  // },
-  // {
-  // {
-  //   title: "animals",
-  //   subreddit: "aww",
-  //   textColor: "",
-  //   bgColor: "#7dcc16"
-  // },
-
-  // {
-  //   title: "gaming",
-  //   subreddit: "games",
-  //   textColor: "",
-  //   bgColor: "#a80872"
-  // },
 ];
-
-// export const icons = {
-//   share: {
-//     title: "",
-//   }
-// };
